b489888edaef2d3cb9ca44566a58592076c45c77
Karen Ling <karensling@gmail.com>
2015-07-04 20:48:10 -0700

    Merge sort.

--- warmup.rb
+++ warmup.rb
@@ -37,30 +37,70 @@
 #
 # p fibonacci(8) #=> [0, 1, 1, 2, 3, 5, 8, 13]
 
-def binary_search(arr, target)
+# def binary_search(arr, target)
+#
+#   if arr.length == 0
+#     return nil
+#   end
+#
+#   middle = arr.length / 2
+#   left = arr.take(middle)
+#   right = arr.drop(middle + 1)
+#
+#   if arr[middle] == target
+#     return middle
+#   elsif target < arr[middle]
+#     binary_search(left, target)
+#   elsif target > arr[middle]
+#     sub_answer = binary_search(right, target)
+#     sub_answer.nil? ? nil : sub_answer + middle + 1
+#   end
+# end
+#
+# p binary_search([1, 2, 3], 1) # => 0
+# p binary_search([2, 3, 4, 5], 3) # => 1
+# p binary_search([2, 4, 6, 8, 10], 6) # => 2
+# p binary_search([1, 3, 4, 5, 9], 5) # => 3
+# p binary_search([1, 2, 3, 4, 5, 6], 6) # => 5
+# p binary_search([1, 2, 3, 4, 5, 6], 0) # => nil
+# p binary_search([1, 2, 3, 4, 5, 7], 6) # => nil
 
-  if arr.length == 0
-    return nil
-  end
 
-  middle = arr.length / 2
-  left = arr.take(middle)
-  right = arr.drop(middle + 1)
+def merge_sort(arr)
+  if arr.length < 2
+    arr
+  else
+    middle = arr.length / 2
+    left = arr.take(middle)
+    right = arr.drop(middle)
 
-  if arr[middle] == target
-    return middle
-  elsif target < arr[middle]
-    binary_search(left, target)
-  elsif target > arr[middle]
-    sub_answer = binary_search(right, target)
-    sub_answer.nil? ? nil : sub_answer + middle + 1
+    merge(merge_sort(left), merge_sort(right))
   end
 end
 
-p binary_search([1, 2, 3], 1) # => 0
-p binary_search([2, 3, 4, 5], 3) # => 1
-p binary_search([2, 4, 6, 8, 10], 6) # => 2
-p binary_search([1, 3, 4, 5, 9], 5) # => 3
-p binary_search([1, 2, 3, 4, 5, 6], 6) # => 5
-p binary_search([1, 2, 3, 4, 5, 6], 0) # => nil
-p binary_search([1, 2, 3, 4, 5, 7], 6) # => nil
+def merge(left, right)
+  sorted = []
+
+  until left.empty? || right.empty?
+    if left.first < right.first
+      sorted << left.shift
+    elsif right.first < left.first
+      sorted << right.shift
+    end
+  end
+
+  sorted + left + right
+end
+
+p merge_sort([4, 3, 6, 2]) #=> [2, 3, 4, 6]
+
+def subsets(arr)
+
+end
+
+
+p subsets([]) # => [[]]
+p subsets([1]) # => [[], [1]]
+p subsets([1, 2]) # => [[], [1], [2], [1, 2]]
+p subsets([1, 2, 3])
+# => [[], [1], [2], [1, 2], [3], [1, 3], [2, 3], [1, 2, 3]]
